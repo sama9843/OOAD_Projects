c4df9ecbabfb73d9c8b5cacb0926a138130a2088
sama9843 <sama9843@colorado.edu>
2022-02-09 13:52:14 -0700

    Added Function to Deals for Customer

--- Customer.java
+++ Customer.java
@@ -1,22 +1,31 @@
+import java.util.Random;
 //abstract class for all customers
 //will only be used to implement buyer and seller in this project
 abstract class Customer{
     //attributes protected by encapsulation
     private String item;
     //used for the deals can delete if not implementing this way
-    private boolean deal1;
-    private boolean deal2;
+    private int deal1;
+    private int deal2;
     //constructor
     public Customer(){
         //TODO
         //set item to buy/sell
         //set deal1 and deal2 using randnum generator
-        this.deal1 = true;
-        this.deal2 = false;
+        Random rand = new Random();
+        this.deal1 = rand.nextInt(2);
+        this.deal2 = rand.nextInt(4);
     }
     //getter methods for deals
-    public boolean getDeal1(){return deal1;}
-    public boolean getDeal2(){return deal2;}
+    //converts the rand num into boolean
+    public boolean getDeal1(){
+        if(deal1!=0){return true;}
+        else{return false;}
+    }
+    public boolean getDeal2(){
+        if(deal2!=0){return true;}
+        else{return false;}
+    }
 }
 //Buyer implements Customer
 class Buyer extends Customer{
